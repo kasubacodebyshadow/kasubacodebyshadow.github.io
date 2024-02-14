cba4aae69c8cbce29bf5ded99c9ada4854269893
kasubacodebyshadow <159984516+kasubacodebyshadow@users.noreply.github.com>
2024-02-14 12:21:21 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,64 @@
+"use strict";
+
+const titleElement = document.querySelector(".title");
+const buttonsContainer = document.querySelector(".buttons");
+const yesButton = document.querySelector(".btn--yes");
+const noButton = document.querySelector(".btn--no");
+const catImg = document.querySelector(".cat-img");
+
+const MAX_IMAGES = 5;
+
+let play = true;
+let noCount = 0;
+
+yesButton.addEventListener("click", handleYesClick);
+
+noButton.addEventListener("click", function () {
+  if (play) {
+    noCount++;
+    const imageIndex = Math.min(noCount, MAX_IMAGES);
+    changeImage(imageIndex);
+    resizeYesButton();
+    updateNoButtonText();
+    if (noCount === MAX_IMAGES) {
+      play = false;
+    }
+  }
+});
+
+function handleYesClick() {
+  titleElement.innerHTML = "Yesssss!!! i lovee youu mahaall!!!";
+  buttonsContainer.classList.add("hidden");
+  changeImage("yes");
+  
+}
+
+function resizeYesButton() {
+  const computedStyle = window.getComputedStyle(yesButton);
+  const fontSize = parseFloat(computedStyle.getPropertyValue("font-size"));
+  const newFontSize = fontSize * 1.6;
+
+  yesButton.style.fontSize = `${newFontSize}px`;
+}
+
+function generateMessage(noCount) {
+  const messages = [
+    "No",
+    "WEEEEEE dingaa??",
+    "Imma give yoo squid pleasee",
+    "Aweee :(",
+    "You don't love me naa :(",
+    "You shall fullfil my wishes",
+  ];
+
+  const messageIndex = Math.min(noCount, messages.length - 1);
+  return messages[messageIndex];
+}
+
+function changeImage(image) {
+  catImg.src = `image/cat-${image}.jpg`;
+}
+
+function updateNoButtonText() {
+  noButton.innerHTML = generateMessage(noCount);
+}
